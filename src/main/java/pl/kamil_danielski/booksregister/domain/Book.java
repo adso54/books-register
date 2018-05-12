edcdddd1c34616d6855df025254a294526002f5a
Kamil <kamil.danielski@gmail.com>
2018-05-12 17:53:06 +0200

    Create domain

--- src/main/java/pl/kamil_danielski/booksregister/domain/Book.java
+++ src/main/java/pl/kamil_danielski/booksregister/domain/Book.java
@@ -1,4 +1,18 @@
 package pl.kamil_danielski.booksregister.domain;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @ManyToOne
+    private Author author;
 }
